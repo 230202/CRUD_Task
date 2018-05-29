52c622752e8b5904a81e2d962cf5fd718a01810c
230202 <230202@shakopeeschools.org>
2018-05-29 08:12:47 -0500

    Update readme

---
+++ update copy.php
@@ -0,0 +1,4 @@
+Created by Aiden Schmitz
+CSS, PHP, HTML are all used in the webpage each in their own files with some having multiple files on cloud 9.
+The website isto be used to store information and records about games and events. You can create new tables, delete, read, and update the information. 
+The webpage can be used to store data from video games as well as long as there are two opposing sides. 
